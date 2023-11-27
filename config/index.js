6306b0d1245c7df5211ced16c9e2ba2137cb4cde
523577556 <152153765+523577556@users.noreply.github.com>
2023-11-27 15:54:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxa8f034833a48f137",
   appsecret: "28f59d4501156af2fd7e6dbc63295870",
   template_id: "ctS_SbobR-vb-thQtrs9FIa21Uukhws8M_zTcMiapvE",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "o6R2C6T6Ai1TReEUPQJ6efPhF-oI",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "南京",
